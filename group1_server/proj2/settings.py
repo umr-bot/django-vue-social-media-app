1e01f8cfbf579cb4f8e98a2956489746b6d08323
18199313 <18199313@sun.ac.za>
2020-05-13 00:59:13 +0200

    Changed database to postgres in settings file

--- group1_server/proj2/settings.py
+++ group1_server/proj2/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'proj2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        "DEFAULT-CHARACTER-SET": 'utf8',
+        'HOST': '',
+        'PORT': 5432,
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Johannesburg'
 
 USE_I18N = True
 
