36b00dde914190917bfd0d70894b19cd22458423
LizaneBotha <19791666@sun.ac.za>
2020-05-17 14:58:25 +0200

    added logo, started routes

--- vue_crash_todolist/src/components/layout/header.vue
+++ vue_crash_todolist/src/components/layout/header.vue
@@ -1,8 +1,12 @@
 <template>
-	<header class="header">
-		<h1>GotYouCovid</h1>
-		<small>The app that connects you to the world when Covid has you stuck in one place.</small>
-	</header>
+	<div id="head-container">
+		<header class="header wrapper clearfix">
+			<div class="center">
+				<h1 id="title">gotY<img src="../../assets/covid19.png" class="logo" alt="" height="70" width="70" />Ucovid</h1>
+			</div>
+			<small>The app that connects you to the world when Covid has you stuck in one place.</small>
+		</header>
+	</div>
 </template>
 
 <script>
@@ -19,8 +23,22 @@ export default {
 		padding: 10px;
 	}
 
-	.header a {
+	.header h1 {
 		color: #fff;
 		padding-right: 5px;
+		display: inline-block;
 	}
+
+	.center { display: table; margin: 0 auto; }
+	img.logo {margin: 0 -12px; vertical-align: middle;}
+
+	.title {
+		padding: 20px;
+		display: inline;
+		font-size: 4.5em;
+		border-top: 4px solid #5dc1c4;
+		border-bottom: 4px solid #5dc1c4; 
+	}
+
+
 </style>
