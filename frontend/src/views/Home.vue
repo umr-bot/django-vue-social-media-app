c9075c663e432c7c862886bb0278c23bc0a0e624
Evan Gilbert <19485972@sun.ac.za>
2020-06-02 21:36:04 +0200

    fixed header vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -8,7 +8,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import Header from './components/Header.vue'
+import Header from '@/components/Header.vue'
 
 export default {
   name: 'Home',
